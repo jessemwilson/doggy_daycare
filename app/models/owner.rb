bcbbb650346f4ccb0e9ad536b29fd79e7a25476d
Jesse Wilson <jessewilson64@gmail.com>
2015-11-11 09:32:42 -0600

    annotated models and added validations

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,2 +1,19 @@
 class Owner < ActiveRecord::Base
+	has_many :dogs
+
+	validates :first_name, :last_name, :phone, :email, :presence:true
 end
+
+# == Schema Information
+#
+# Table name: owners
+#
+#  id          :integer          not null, primary key
+#  first_name  :string
+#  last_name   :string
+#  phone       :string
+#  email       :string
+#  evac_waiver :boolean
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
