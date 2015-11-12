9031a6e2f574d8fbdd45bd6a2a26916bb9fa65d0
Jesse Wilson <jessewilson64@gmail.com>
2015-11-12 08:28:15 -0600

    updated bootstrap style/javascript cdn's from 3.0.3 to 3.3.5, added first name to owner collection select in dog form page

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -2,6 +2,10 @@ class Owner < ActiveRecord::Base
 	has_many :dogs
 
 	validates :first_name, :last_name, :phone, :email, presence:true
+
+	def last_comma_first
+		"#{last_name}, #{first_name}"
+	end
 end
 
 #TODO add phone number validation
